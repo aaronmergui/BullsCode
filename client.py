0697e9cd330b524c6af4664a67567a4fff755b3b
aaronmergui <merguiaaron@gmail.com>
2023-12-20 14:45:59 +0200

    step 2 finished v2

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import socket
 from bullscode import BullsCode
-
-IP_ADRESS = "192.168.1.81"
+import time
+IP_ADRESS = " 192.168.1.27"
 IP_LOCAL = "127.0.0.1"
 PORT = 2289
 
@@ -10,18 +10,30 @@ def game():
     my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     my_socket.connect((IP_LOCAL, PORT))
     t = 0
-    while t != 10:
+    start=0
+    win = False
+    while t != 10 or win == True:
         gess = input("enter your guess: ")
+        if t==0:
+            start=time.time()
         my_socket.send(gess.encode())
         result = my_socket.recv(1024).decode()
+        if result == "close":
+            break
         print("the server sent: " + result)
         if (result == "[B, B, B, B]"):
             print("win")
-            break
+            win = True
         if t == 9:
             print("loose")
         t += 1
+        if time.time()-start>=30:
+            break
 
+        while win == True:
+            continue
+
+    my_socket.send("".encode())
     my_socket.close()
 
 
--- server.py
+++ server.py
@@ -1,33 +1,85 @@
 import socket
-import random
+import select
+
 from bullscode import BullsCode
 
 ADDRESS = "0.0.0.0"
 PORT = 2289
 
 
+class Player:
+    def __init__(self, socket,address):
+        self.socket = socket
+        self.address = address
+        self.win=False
+        self.attempts = 0
+
+def print_client_sockets(client_sockets):
+     for c in client_sockets:
+         print("\t", c.getpeername())
+def client_gestion(player,random_answer,client_guess):
+
+        result = random_answer.check(BullsCode(client_guess))
+        result = "[" + ', '.join(result) + "]"
+        player.socket.send(result.encode())
+        if result == "[B, B, B, B]":
+            print("player won ")
+            player.win=True
+            player.attempts += 1
+            return True
+        if player.attempts == 9:
+            print("Computer won!")
+        player.attempts+=1
+        return False
+
+def find_player(players,socket):
+    for player in players:
+        if player.socket == socket:
+            return player
+
 def game():
+    print("Setting up server...")
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((ADDRESS, PORT))
     server_socket.listen()
     print("server listening")
-    client_socket, client_adress = server_socket.accept()
-    print("player connected")
-    t = 0
-    random_answer = BullsCode()
-    while t != 10:
-        client_gess = client_socket.recv(1024).decode()
-        result = random_answer.check(BullsCode(client_gess))
-        result = "["+', '.join(result)+"]"
-        client_socket.send(result.encode())
-        if result == "[B, B, B, B]":
-            print("player won ")
-            break
-        if t == 9:
-            print("Computer won!")
-        t += 1
+    players=[]
+    client_sockets=[]
+    random_answer=BullsCode()
+    print(random_answer.code)
 
-    client_socket.close()
+    while True:
+        ready_to_read, _ready_to_write, _in_error = select.select([server_socket]+ client_sockets, [], [])
+        for  ready_socket in ready_to_read:
+            if ready_socket == server_socket:
+                client_socket, client_address = server_socket.accept()
+                print(f"new Player connected  {client_address}")
+                new_player = Player(client_socket, client_address)
+                players.append(new_player)
+                client_sockets.append(client_socket)
+                print_client_sockets(client_sockets)
+
+            else:
+                print("Data from a client arrived")
+                player= find_player(players, ready_socket)
+                data = ready_socket.recv(1024).decode()
+
+                if data=="":
+                    print("connection closed")
+                    client_sockets.remove(ready_socket)
+                    players.remove(player)
+                    ready_socket.close()
+                else:
+                    if client_gestion(player,random_answer,data)==True:
+                        for x in players:
+                            if x!=player:
+                                x.socket.send("close".encode())
+                                x.socket.close()
+                                players.remove(x)
+                                client_sockets.remove(x.socket)
+                                print("connection closed")
+
+
 
 
 if __name__ == '__main__':
